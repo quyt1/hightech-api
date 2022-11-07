d34439bb606b8ad8f21638fa2e25efc3abe4fe01
Quy Thanh <quythanh24@gmail.com>
2022-11-07 10:34:37 +0700

    no message

--- middle/error-handler.js
+++ middle/error-handler.js
@@ -1,9 +1,12 @@
 let _ = require('lodash');
 const Sentry = require('@sentry/node');
+require('dotenv').config();
 
 module.exports = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next))
         .catch(err => {
+            const status = err.status || 500;
+            if (status >= 500 && process.env.FLAVOR !== 'dev') {
                 /*
                 * Sentry report
                 */
@@ -16,6 +19,8 @@ module.exports = fn => (req, res, next) => {
                     scope.setExtra("errors", err);
                     Sentry.captureException(err);
                 });
+            }
+
             console.log(err);
             next(err);
         });
