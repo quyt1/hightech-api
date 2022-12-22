cb69287edc85d159ed55109b9e7e2aea681adefe
Quy Thanh <quythanh24@gmail.com>
2022-12-22 22:53:01 +0700

    fix bug

--- modules/app-device-management/routes.js
+++ modules/app-device-management/routes.js
@@ -4,7 +4,6 @@ var router = express.Router();
 const ErrorHandler = require('../../middle/error-handler');
 const { Auth } = require('../../middle/AuthMiddleware')
 
-router.use(Auth)
 router.post('/', ErrorHandler(controller.create));
 
 module.exports = router
